59cda8b475662109d38fab064ad8a6e25fd0c8d1
wlsgb <wlsgb94@naver.com>
2020-06-04 14:55:42 +0900

    컨트롤러이름변경, 상단 고객센터 버튼작동변경

--- project_Dank/src/dank/mvc/model/CertifiedController.java
+++ project_Dank/src/dank/mvc/model/CertifiedController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UjaeController {
+public class CertifiedController {
 
 	@RequestMapping(value= "/login")
 	public String u1() {
