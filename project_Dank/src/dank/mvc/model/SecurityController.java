4e99d94fddbc3732eecff48b2dc7750bbcf79fc0
wlsgb <wlsgb94@naver.com>
2020-06-07 15:44:32 +0900

    SecurityController 별도

--- project_Dank/src/dank/mvc/model/SecurityController.java
+++ project_Dank/src/dank/mvc/model/SecurityController.java
@@ -10,5 +10,28 @@ import dank.mvc.method.Mail;
 @Controller
 public class SecurityController {
 
-	
+	@RequestMapping(value = "/security")
+	public String viewSecurity() {
+		return "myinfo/security";
+	}
+
+	@RequestMapping(value = "/securitycard")
+	public String viewSecurity_card() {
+		return "myinfo/securityCard";
+	}
+
+	@RequestMapping(value = "/securitycardsuccess")
+	public String viewSecurity_card_success() {
+		return "myinfo/securityCardSuccess";
+	}
+
+	@RequestMapping(value = "/securityotp")
+	public String viewSecurity_otp() {
+		return "myinfo/securityOtp";
+	}
+
+	@RequestMapping(value = "/securityotpsuccess")
+	public String viewCheckBalance() {
+		return "myinfo/securityOtpSuccess";
+	}
 }
