4272c019594ea2e0c4c64a42616423fe890e78b4
KOSMO_04 <KOSMO_04@DESKTOP-DIN080J>
2020-06-09 18:40:48 +0900

    커밋수정

--- project_Dank/src/dank/mvc/dao/MemberDao.java
+++ project_Dank/src/dank/mvc/dao/MemberDao.java
@@ -7,6 +7,8 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import dank.mvc.vo.MemberVO;
+
 
 @Repository
 public class MemberDao {
