fa58f9aa553ab258bf88de4afd31c4ee3e051433
kosmo_08 <kosmo_08@DESKTOP-CKODRL0>
2020-07-27 10:34:41 +0900

    a

--- project_Dank/src/dank/mvc/method/SmsProperties.java
+++ project_Dank/src/dank/mvc/method/SmsProperties.java
@@ -1,11 +1,17 @@
 package dank.mvc.method;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class SmsProperties {
-	
+	String api_key = "#ENTER_YOUR_OWN#";
+	  String api_secret = "#ENTER_YOUR_OWN#";
+
+	  
 	@RequestMapping(value = "/sendSMS")
 	public String sendSMS(String userPhoneNumber) {
 		String api_key="NCSNJETH2DZH45LE";
